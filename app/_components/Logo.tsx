b23a1ebfb9ff675343616caca4c8583b6751de48
Yuri Viana <ycviana@sga.pucminas.br>
2026-08-26 14:13:03 -0300

    feat: criar logo do header

---
+++ app/_components/Logo.tsx
@@ -0,0 +1,39 @@
+const Logo = () => {
+  return (
+    <svg
+      width="45"
+      height="26"
+      viewBox="0 0 45 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs>
+        <linearGradient
+          id="logoGradient"
+          x1="0"
+          y1="0"
+          x2="45"
+          y2="26"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#5A75F6" />
+          <stop offset="1" stopColor="#8D60F7" />
+        </linearGradient>
+      </defs>
+
+      {/* Y */}
+      <path
+        d="M0 0H7L11 8L15 0H22L14 14V25H8V14L0 0Z"
+        fill="url(#logoGradient)"
+      />
+
+      {/* V */}
+      <path
+        d="M23 0H30L34.5 17L39 0H45L37 25H31L23 0Z"
+        fill="url(#logoGradient)"
+      />
+    </svg>
+  );
+};
+
+export default Logo;
