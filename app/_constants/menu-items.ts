a8266b276a445557fd081a35bfd99d2aa11ce020
Yuri Viana <ycviana@sga.pucminas.br>
2026-08-26 19:31:35 -0300

    feat: criar constantes utilitárias

---
+++ app/_constants/menu-items.ts
@@ -0,0 +1,34 @@
+import { Home, User, Monitor, BriefcaseBusiness, MessageSquare, Activity } from "lucide-react";
+
+export const menuItems = [
+  {
+    label: "Início",
+    href: "/",
+    icon: Home,
+  },
+  {
+    label: "Sobre",
+    href: "/about",
+    icon: User,
+  },
+  {
+    label: "Projetos",
+    href: "/projects",
+    icon: Monitor,
+  },
+  {
+    label: "Experiência",
+    href: "/experience",
+    icon: BriefcaseBusiness,
+  },
+  {
+    label: "Habilidades",
+    href: "/abilities",
+    icon: Activity,
+  },
+  {
+    label: "Contato",
+    href: "/contact",
+    icon: MessageSquare,
+  },
+];
---
+++ app/_constants/project.ts
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    title: "Help Desk",
+    description: "Plataforma de gestão de chamados com controle de acesso, cálculo de custos e relatórios.",
+    skills: ["Next.js", "TypeScript", "Prisma", "React", "Node.js"],
+    href: "",
+    image: "/projects/HelpDesk.png",
+    date: "12/2025",
+    isLatest: true
+  },
+  {
+    title: "Moovie App",
+    description: "Aplicação para busca, favoritos e avaliações de filmes com interface moderna.",
+    skills: ["React", "Node.js", "SQLite", "TypeScript"],
+    href: "",
+    image: "/projects/MoovieApp.png",
+    date: "10/2025",
+    isLatest: false
+  }
+]
