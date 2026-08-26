89f6ac6cefee2034300f2d30f864295aa890fec4
Yuri Viana <ycviana@sga.pucminas.br>
2026-08-26 14:14:48 -0300

    feat: finalizar a home

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Oxanium } from "next/font/google";
 import "./globals.css";
+import Header from "./_components/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const oxanium = Oxanium({
+  variable: "--font-oxanium",
 });
 
 export const metadata: Metadata = {
@@ -20,10 +15,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      lang="pt-BR"
+      className={`${oxanium.className} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col [&::-webkit-scrollbar]:hidden">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,77 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Technologies from "./_components/Technologies";
+import TextPurpleRadial from "./_components/TextPurpleRadial";
+import ProjectCard from "./_components/ProjectCard";
+import Link from "next/link";
+
+const projects = [
+  {
+    title: "Help Desk",
+    description: "Plataforma de gestão de chamados com controle de acesso, cálculo de custos e relatórios.",
+    skills: ["Next.js", "TypeScript", "Prisma", "React", "Node.js"],
+    href: "",
+    image: "/projects/HelpDesk.png"
+  },
+  {
+    title: "Moovie App",
+    description: "Aplicação para busca, favoritos e avaliações de filmes com interface moderna.",
+    skills: ["React", "Node.js", "SQLite", "TypeScript"],
+    href: "",
+    image: "/projects/MoovieApp.png"
+  }
+]
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+  return (  
+    <div className="bg-[#050714] min-h-screen ">
+      <div className="relative h-150 w-full overflow-visible">
         <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          src="/bg-mobilee-home.png"
+          alt="Tela de fundo da página inicial"
+          fill
+          className="object-cover object-[20%_80%]"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
+        <div className="absolute inset-0 bg-linear-to-b from-[#050714] via-[#050714]/20 to-[#050714]" />
+        <div className="absolute top-10 z-10 w-full px-5">
+          <p className="text-[#90A1B9]">Olá, eu sou o</p>
+          <h1 className="mt-2 mb-3 text-4xl font-bold text-white">
+            Yuri{" "}
+            <TextPurpleRadial text="Viana" />
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+          <h2 className="text-lg text-white">Desenvolvedor Full Stack</h2>
+          <p className="mt-3 text-white ">
+            Transformo ideias em <br />
+            <TextPurpleRadial className="font-semibold" text="experiências digitais" />
           </p>
+          <div className="mt-7.5 flex items-center gap-2">
+            <Button className="bg-linear-to-l border-2 border-[#7C3AED] from-[#7C3AED] to-[#4F46E5] px-5 py-[20px]">
+              Ver meus projetos
+            </Button>
+            <Button className="border-2 border-[#8B5CF6] bg-transparent px-5 py-[20px]">
+              Sobre mim
+            </Button>
+          </div>
+          <div className="mt-40">
+            <Technologies />
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
+
+      <h3 className="text-[#E2E8F0] px-5 font-bold text-3xl mt-10">
+        Alguns dos meus {" "} <br />
+        <TextPurpleRadial text="trabalhos" />
+      </h3>
+
+      <div className="px-5 mt-10 space-y-6">
+        {
+          projects.map((project) => (
+            <Link href={project.href} key={project.title} className="inline-flex">
+              <ProjectCard description={project.description} href={project.href} image={project.image} skills={project.skills} title={project.title} />
+            </Link>
+          ))
+        }
+      </div>
     </div>
   );
 }
