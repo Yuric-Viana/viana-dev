98e27c453020deb66f1b10d6c066cddb7995f86c
Yuri Viana <ycviana@sga.pucminas.br>
2026-09-01 10:31:46 -0300

    feat: criar array de objetos para estátisticas

---
+++ app/_constants/stats-data.ts
@@ -0,0 +1,18 @@
+export const statsData = [
+  {
+    number: "2+",
+    description: "Anos estudando",
+  },
+  {
+    number: "10+",
+    description: "Projetos concluídos",
+  },
+  {
+    number: "6+",
+    description: "Tecnologias",
+  },
+  {
+    number: "∞",
+    description: "Sempre aprendendo",
+  },
+];
