a0bf5f3da1dc1d43f93cbded562459216cdf5a5f
Yuri Viana <ycviana@sga.pucminas.br>
2026-09-02 20:51:30 -0300

    feat: adicionar background de tela global e adicionar sidebar desktop

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Oxanium } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
+import { StarsBackground } from "@/components/StarsBackground";
+import SidebarDesktop from "@/components/SidebarDesktop";
 
 const oxanium = Oxanium({
   variable: "--font-oxanium",
@@ -18,9 +20,17 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="pt-BR"
       className={`${oxanium.className} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col [&::-webkit-scrollbar]:hidden bg-[#050714]">
-        <Header />
-        {children}
+      <body className="min-h-full flex flex-col [&::-webkit-scrollbar]:hidden bg-[#050714] overflow-x-hidden">
+        <StarsBackground />
+        <div className="lg:grid lg:grid-cols-[8%_auto]">
+          <div className="border-r border-[#8B5CF6]/10 hidden lg:block">
+            <SidebarDesktop />
+          </div>
+          <div>
+            <Header />
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
